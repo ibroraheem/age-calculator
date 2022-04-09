dbdb5d9c584a01efb5246ab5a3b7252d9f6f37cb
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2022-04-09 17:36:32 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+function age(){
+
+    let birthDay = document.getElementById('date').value;
+    let birthMonth = document.getElementById('month').value;
+    let birthYear = document.getElementById('year').value;
+    let date = new Date();
+    let currentDate = date.getDate();
+    let currentMonth = 1 + date.getMonth();
+    let currentYear = date.getFullYear();
+    let month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    
+    if (birthDay > currentDate) {
+        currentDate = currentDate + month[2] - 1;
+        currentMonth = currentMonth - 1;
+    }
+    
+    if (birthMonth > currentMonth) {
+        currentMonth = currentMonth + 12;
+        currentYear = currentYear - 1;
+    }
+    
+    let d = currentDate - birthDay;
+    let m = currentMonth - birthMonth;
+    let y = currentYear - birthYear;
+    
+    document.getElementById('age').innerHTML = 'Your Age is ' + y + ' Years ' + m + ' Months ' + d + ' Days';
+}
